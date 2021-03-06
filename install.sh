f64b102705e83ab431a83b015f59c6555d7bab0d
Alejandro Colon <Alejandro@alejandroc.com>
2021-03-06 13:05:49 -0500

    backup

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Clone succeeded"
 echo "Starting install"
 if ! grep -q '$HOME/linux-config' $HOME/.bashrc; then
 	cat ~/linux-config/.bashrc >> ~/.bashrc
-	echo "Inserting personal file reference into bashrc"
+	echo "Installing personal file reference into bashrc"
 fi
 if ! grep -q 'linux-config' $HOME/.vimrc 2> /dev/null; then
 	cat ~/linux-config/.vimrc_config >> ~/.vimrc
-	echo "Inserting personal file reference in vimrc"
+	echo "Installing personal file reference into vimrc"
 fi
 mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/bundle
@@ -19,7 +18,7 @@ if [ ! -f "~/.vim/autoload/pathogen.vim" ] ; then
 fi
 ## if ! grep -q 'git' $HOME/.vim/bundle/vim-json 2> /dev/null; then
 if [ ! -f "~/.vim/bundle/vim-json/.git" ]; then
-	echo "vim-json is not installed...Installing now"
+	echo "Installing vim-json for vim"
 	git clone -q https://github.com/Ajhad1/vim-json.git ~/.vim/bundle/vim-json
 else
 	git --git-dir=$HOME/.vim/bundle/vim-json/.git --work-tree=$HOME/.vim/bundle/vim-json/ pull
